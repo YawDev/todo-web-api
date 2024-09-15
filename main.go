6b7c0dccc3e7c3bd7d3656f4369161038ba81f52
YawDev <94807175+YawDev@users.noreply.github.com>
2024-09-14 21:15:40 -0400

    Refactor

--- main.go
+++ main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	auth "todo-web-api/auth"
+	app "todo-web-api/todo"
 
 	gin "github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+	RouteSetup(r)
+	r.Run()
+}
+
+func RouteSetup(r *gin.Engine) {
 	r.GET("/Login", auth.Login)
 	r.GET("/Register", auth.Register)
-	r.Run()
+	r.GET("/Home", app.Home)
 }

04fd576b83103e7c1d6708be0a4d6dd44a15f850
YawDev <94807175+YawDev@users.noreply.github.com>
2024-10-28 13:34:05 -0400

    Unit Test User Create

--- Tests/userStore_test.go
+++ Tests/userStore_test.go
@@ -6,6 +6,7 @@ import (
 	"todo-web-api/Models"
 	"todo-web-api/Storage"
 
+	"github.com/stretchr/testify/assert"
 	"gopkg.in/DATA-DOG/go-sqlmock.v1"
 )
 
@@ -26,4 +27,9 @@ func Test_Create_UserExists(t *testing.T) {
 		WithArgs(testUser.Username, testUser.CreatedAt).
 		WillReturnResult(sqlmock.NewResult(2, 1)) // Returning new user ID
 	mock.ExpectCommit()
+
+	success, _ := Storage.UserManager.CreateUser(&Models.User{Id: 1, Username: "TestUser", Password: "Test_PW1", CreatedAt: time.Now()})
+
+	assert.Equal(t, success, 0)
+
 }
